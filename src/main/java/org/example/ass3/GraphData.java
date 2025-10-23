c3e1739476325a498ac1c7327f85804102f07b20
Shagarbek <shagarbekov@gmail.com>
2025-10-23 21:20:59 +0500

    feat: complete GraphData class with vertices and edges

--- src/main/java/org/example/ass3/GraphData.java
+++ src/main/java/org/example/ass3/GraphData.java
@@ -1,4 +1,15 @@
 package org.example.ass3;
 
+import java.util.List;
+
 public class GraphData {
+    public String name;
+    public List<String> vertices;
+    public List<EdgeData> edges;
+
+    public static class EdgeData {
+        public String from;
+        public String to;
+        public double weight;
+    }
 }
